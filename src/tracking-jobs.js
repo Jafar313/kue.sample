b11d3693dc7cd1f83dc3b9d0ed96aa5913a94d6b
Seyed Jafar Mousavi <j.mosavi2@gmail.com>
2023-07-03 18:35:34 +0330

    adding job with delay

--- src/tracking-jobs.js
+++ src/tracking-jobs.js
@@ -7,18 +7,14 @@ const jobType = 'job-tracer';
 let n = 5;
 while (n > 0) {
     n--;
-    const job = queue.create(jobType, {id: Math.random(), weight: 'very heavy data'}).save();
-
-    console.log(`job created. job.data.id: `, job.data.id);
-
-    job.on(events.start, (typeOfJobStarted) => {
-        console.log(`job started. type of job started: `, typeOfJobStarted);
-    });
-
-    job.on(events.complete, (result) => {
-        console.log(`job completed. result: `, result);
-    });
+    createNewJob();
 }
+
+
+setTimeout(() => {
+    console.log('create a new job after a wild started');
+    createNewJob();
+}, 5 * 1000);
 
 queue.process(jobType, (job, done) => {
     /**
@@ -32,3 +28,17 @@ queue.process(jobType, (job, done) => {
         done && done(null, `processing job ${job.data.id} completed successfully`);
     }, 3 * 1000);
 })
+
+function createNewJob() {
+    const job = queue.create(jobType, {id: Math.random(), weight: 'very heavy data'}).save();
+
+    console.log(`job created. job.data.id: `, job.data.id);
+
+    job.on(events.start, (typeOfJobStarted) => {
+        console.log(`job started. type of job started: `, typeOfJobStarted);
+    });
+
+    job.on(events.complete, (result) => {
+        console.log(`job completed. result: `, result);
+    });
+}
